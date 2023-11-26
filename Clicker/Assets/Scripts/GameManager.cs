b92eee924022c028b0c5d508c7200031e9ca2666
Beam-K <kbuldina415@gmail.com>
2023-11-26 18:32:48 +0700

    Сhange in scripts

--- Clicker/Assets/Scripts/GameManager.cs
+++ Clicker/Assets/Scripts/GameManager.cs
@@ -1,25 +1,45 @@
-using System;
 using UnityEngine;
-using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Serialization;
 
+
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private int _score;
-    [SerializeField] private int clickCost = 1;
+    [SerializeField] private int _score ; 
+    [SerializeField] private int _clickCost = 1;
+    [SerializeField] private GameManager _gameManager;
 
     [SerializeField] private TMP_Text scoreTMPro;
-
-
-    private void Update()
+ 
+ public void Click()
     {
+        _score += _clickCost;
         scoreTMPro.text = _score.ToString();
+        
     }
 
-    public void Click()
-    {
-        _score += clickCost;
-    }
+ public int GiveScore()
+ {
+     return _score;
+ }
+
+ public int ReturnScore()
+ {
+     _gameManager.ReturnScore();
+     _score = _gameManager.ReturnScore();
+     return _score;
+ }
+ 
+ public int ReturnClickCost()
+ {
+     _gameManager.ReturnScore();
+     _clickCost = _gameManager.ReturnScore();
+     return _clickCost;
+
+ }
+ public int GiveClickCost()
+ {
+     return _clickCost;
+ }
 }
 
--- Clicker/Assets/Scripts/UpgradeClick.cs
+++ Clicker/Assets/Scripts/UpgradeClick.cs
@@ -1,19 +1,45 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class UpgradeClick : MonoBehaviour
 {
-    [SerializeField] private int _upgradeClick = 10;
+    [SerializeField] private int _upgradeCost = 10;
+    [SerializeField] private GameManager _gameManager;
+    private int _score;
+    private int _clickCost;
     
     [SerializeField] private TMP_Text upgradeScoreText;
 
     
-    public void Update()
+    public void ShowUpgradeScoreCost()
     {
-        upgradeScoreText.text = _upgradeClick.ToString();
+        upgradeScoreText.text = _upgradeCost.ToString();
     }
     
-}
+    
+
+    public void UpgrateClick()
+    {
+        _gameManager.GiveScore();
+        _score = _gameManager.GiveScore();
+
+        _gameManager.GiveScore();
+        _clickCost = _gameManager.GiveScore();
+        if (_score >= _upgradeCost)
+        {
+            _score -= _upgradeCost;
+            _upgradeCost += 20;
+            _clickCost++;
+        }
+    }
+
+    public int ReturnScore()
+    {
+        return _score;
+    }
+    public int ReturnClickCost()
+    {
+        return _clickCost;
+    }
+} 
